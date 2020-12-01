62bdef66fac8b540d578521e115f740e28aa558a
Ariel Alvear <ariel.alvear.l@gmail.com>
2020-12-01 19:18:09 -0300

    correcciones menores, a partir de comentarios en clases

--- cuentas_bancarias.rb
+++ cuentas_bancarias.rb
@@ -1,4 +1,5 @@
-class Bank_User
+#no ocupar guion y guion bajo para nombrar clases.
+class BankUser
     attr_accessor :name, :bank_accounts
     def initialize(name, bank_accounts = nil)
         @name = name
@@ -7,12 +8,11 @@ class Bank_User
     end
 
     def total_user_balance
-        total_balance =  bank_accounts.map { |b| b.balance }.sum
-        total_balance
+        bank_accounts.map { |b| b.balance }.sum
     end
 end
 
-class Bank_Account
+class BankAccount
     attr_accessor :bank_name, :account_number, :balance
     def initialize(bank_name, account_number, balance = 0)
         @bank_name = bank_name
@@ -28,12 +28,12 @@ end
 
 
 #probamos las transferencias
-cuenta1 = Bank_Account.new('Banco de Chile', '12345678')
-cuenta2 = Bank_Account.new('Itau', '87654321', 5000)
+cuenta1 = BankAccount.new('Banco de Chile', '12345678')
+cuenta2 = BankAccount.new('Itau', '87654321', 5000)
 cuenta2.transfer_money(5000, cuenta1)
 
 #probamos la cardinalidad. 1 persona puede tener N cuentas. Cada cuenta pertenece a solo 1 persona.
-usuario1 = Bank_User.new('ariel', cuenta1)
+usuario1 = BankUser.new('ariel', cuenta1)
 usuario1.bank_accounts.push(cuenta2)
 
 #probamos el método que nos trae el balance total de un usuario
